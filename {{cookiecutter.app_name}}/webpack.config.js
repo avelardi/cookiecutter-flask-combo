dafc9eec0eaf8971346261211475f7a9975a05c4
Tony Velardi <{ID}+{username}@users.noreply.github.com>
2025-04-24 17:48:10 -0400

    webpack expose jquery

--- {{cookiecutter.app_name}}/webpack.config.js
+++ {{cookiecutter.app_name}}/webpack.config.js
@@ -78,6 +78,30 @@ module.exports = {
         }
       },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ["@babel/preset-env"], cacheDirectory: true } },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+      {
+        test: require.resolve("underscore"),
+        loader: "expose-loader",
+        options: {
+          exposes: [
+            "_.map|map",
+            {
+              globalName: "_.reduce",
+              moduleLocalName: "reduce",
+            },
+            {
+              globalName: ["_", "filter"],
+              moduleLocalName: "filter",
+            },
+          ],
+        },
+      },
     ],
   }
 };
